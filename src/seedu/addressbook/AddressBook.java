7f1e67710a05427d1aed046a2d2d7bac7a47dc17
Yesha Simaria <yesha.simaria@u.nus.edu>
2017-01-31 02:00:55 +0800

    Modified comments

--- src/seedu/addressbook/AddressBook.java
+++ src/seedu/addressbook/AddressBook.java
@@ -444,7 +444,7 @@ public class AddressBook {
 
     /**
      * Finds and lists all persons in address book whose name contains any of the argument keywords.
-     * Keyword matching is case sensitive.
+     * Keyword matching is case insensitive.
      *
      * @param commandArgs full command args string from the user
      * @return feedback display message for the operation result
